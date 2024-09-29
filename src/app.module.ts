982bb882ec003733833896139f80858b82957880
Pietro <menchettipietro547@gmail.com>
2024-09-29 10:44:38 +0200

    typeORM supabase local setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 54322,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
